d3dbed3622c52d38fdfe77790ca368f54fe10c69
Sahil More <SEITBS1951119@dypakurdi.onmicrosoft.com>
2021-12-28 12:55:28 +0530

    started with featuire component

--- src/containers/whatGPT3/WhatGPT3.js
+++ src/containers/whatGPT3/WhatGPT3.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Feature } from '../../components'
 import "./whatGPT3.css"
 
 const WhatGPT3 = () => {
     return (
-        <div>
-            <h1>What GPT3</h1>
+      <div className="gpt3_whatgpt3 s section_margin" id="wgpt3">
+        <div className="gpt3_whatgpt3-feature">
+            <Feature/>
         </div>
+            <div className="gpt3_whatgpt3-heading">
+                <h1 className="gradient-text">
+                    CSS just looks simple but its beyong your imagination!
+                </h1>
+                <p>Explore the library</p>
+            </div>
+
+            <div className="gpt3_whatgpt3-container">
+                <Feature />
+            </div>
+      
+      </div>
     )
 }
 
