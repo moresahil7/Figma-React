1def5e56637e1cb560d65c518a3a6028790a7eed
Sahil More <SEITBS1951119@dypakurdi.onmicrosoft.com>
2022-01-03 23:45:00 +0530

    created blog section

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ from "./containers"
 import "./App.css"
 
 const App = () => {
+
+
+
   return (
 
     <div className="App">
--- src/components/cta/CTA.js
+++ src/components/cta/CTA.js
@@ -3,8 +3,14 @@ import "./cta.css"
 
 const CTA = () => {
     return (
-        <div>
-            <h1>CTA</h1>
+        <div className="gpt3_cta section_margin">
+            <div className="gpt3_cta-content">
+                <p>Request Early access to get 40% discount!</p>
+                <h3>Register today and start your coding journey with us!</h3>
+            </div>
+            <div className="gpt3_cta-button">
+            <button>Get Started</button>
+            </div>
         </div>
     )
 }
