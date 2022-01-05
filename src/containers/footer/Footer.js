dccb63d421c0427272977f5f607d1a66740ddcaf
Sahil More <SEITBS1951119@dypakurdi.onmicrosoft.com>
2022-01-05 18:05:58 +0530

    completed blog section

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -3,8 +3,15 @@ import "./footer.css"
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className="gpt3_footer section_padding">
+        <div className="gpt3_footer-heading">
+            <h1 className="gradient_text">
+                Do you want to step in to the future before others!
+            </h1>
+        </div>
+        <div className="gpt3_footer-btn">
+            <p>Request Early Access</p>
+        </div>
         </div>
     )
 }
